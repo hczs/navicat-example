962f2db6144f79eaed4b0ad3f277864f725fb3cb
houcheng <houcheng@boco.com.cn>
2022-09-24 11:24:31 +0800

    及时close connection，归还连接到连接池中

--- src/main/java/icu/sunnyc/navicat/example/controller/DataSourceController.java
+++ src/main/java/icu/sunnyc/navicat/example/controller/DataSourceController.java
@@ -8,6 +8,8 @@ import icu.sunnyc.navicat.example.service.DataSourceService;
 import icu.sunnyc.navicat.example.utils.DbPoolUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,7 @@ import java.sql.Connection;
  * @version V1.0
  * @date 2022/9/23 14:28
  */
+@Slf4j
 @Api(tags = "数据源管理")
 @RestController
 @RequestMapping("/datasource")
@@ -37,17 +40,21 @@ public class DataSourceController {
 
     @PostMapping("/connect")
     @ApiOperation(value = "通过数据源信息连接数据库")
+    @SneakyThrows
     public CommonResult connect(@RequestBody DataSourceDTO dataSource) {
-        Connection connection = DbPoolUtil.getConnection(dataSource.getHost(), dataSource.getPort(),
-                dataSource.getUsername(), dataSource.getPassword());
-        return connection == null ? CommonResult.failed("数据库连接失败") : CommonResult.success().setMessage("数据库连接成功！");
+        try (Connection connection = DbPoolUtil.getConnection(dataSource.getHost(), dataSource.getPort(),
+                dataSource.getUsername(), dataSource.getPassword())) {
+            return connection == null ? CommonResult.failed("数据库连接失败") : CommonResult.success().setMessage("数据库连接成功！");
+        }
     }
 
     @GetMapping("/connect/{dataSourceId}")
     @ApiOperation(value = "通过数据源ID连接数据库")
+    @SneakyThrows
     public CommonResult connect(@PathVariable("dataSourceId") Long dataSourceId) {
-        Connection connection = dataSourceService.connectionByDataSourceId(dataSourceId);
-        return connection == null ? CommonResult.failed("数据库连接失败") : CommonResult.success().setMessage("数据库连接成功！");
+        try(Connection connection = dataSourceService.connectionByDataSourceId(dataSourceId)) {
+            return connection == null ? CommonResult.failed("数据库连接失败") : CommonResult.success().setMessage("数据库连接成功！");
+        }
     }
 
     @GetMapping("/list")
--- src/main/java/icu/sunnyc/navicat/example/controller/ProcessController.java
+++ src/main/java/icu/sunnyc/navicat/example/controller/ProcessController.java
@@ -7,6 +7,8 @@ import icu.sunnyc.navicat.example.service.DataSourceService;
 import icu.sunnyc.navicat.example.utils.DbPoolUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +25,7 @@ import java.util.Map;
  * @version V1.0
  * @date 2022/9/23 16:40
  */
+@Slf4j
 @RestController
 @Api(tags = "线程管理")
 @RequestMapping("/process")
@@ -33,32 +36,36 @@ public class ProcessController {
 
     @GetMapping("/list/{dataSourceId}")
     @ApiOperation(value = "查询指定数据源下所有线程信息")
+    @SneakyThrows
     public CommonResult list(@PathVariable("dataSourceId") Long dataSourceId) {
-        Connection connection = dataSourceService.connectionByDataSourceId(dataSourceId);
-        List<Map<String, Object>> maps = DbPoolUtil.executeQueryList(connection, CommandConstant.QUERY_PROCESS_LIST);
-        ArrayList<ProcessVO> processInfoList = new ArrayList<>();
-        for (Map<String, Object> map : maps) {
-            ProcessVO processInfo = ProcessVO.builder()
-                    .id((Long) map.get("Id"))
-                    .user(String.valueOf(map.get("User")))
-                    .host(String.valueOf(map.get("Host")))
-                    .db(String.valueOf(map.get("db")))
-                    .command((String.valueOf(map.get("Command"))))
-                    .time(String.valueOf(map.get("Time")))
-                    .state(String.valueOf(map.get("State")))
-                    .info(String.valueOf(map.get("info")))
-                    .build();
-            processInfoList.add(processInfo);
+        try (Connection connection = dataSourceService.connectionByDataSourceId(dataSourceId)) {
+            List<Map<String, Object>> maps = DbPoolUtil.executeQueryList(connection, CommandConstant.QUERY_PROCESS_LIST);
+            ArrayList<ProcessVO> processInfoList = new ArrayList<>();
+            for (Map<String, Object> map : maps) {
+                ProcessVO processInfo = ProcessVO.builder()
+                        .id((Long) map.get("Id"))
+                        .user(String.valueOf(map.get("User")))
+                        .host(String.valueOf(map.get("Host")))
+                        .db(String.valueOf(map.get("db")))
+                        .command((String.valueOf(map.get("Command"))))
+                        .time(String.valueOf(map.get("Time")))
+                        .state(String.valueOf(map.get("State")))
+                        .info(String.valueOf(map.get("info")))
+                        .build();
+                processInfoList.add(processInfo);
+            }
+            return CommonResult.success(processInfoList);
         }
-        return CommonResult.success(processInfoList);
     }
 
     @GetMapping("/kill/{dataSourceId}/{processId}")
     @ApiOperation(value = "终止指定线程")
+    @SneakyThrows
     public CommonResult kill(@PathVariable("dataSourceId") Long dataSourceId,
                              @PathVariable("processId") Long processId) {
-        Connection connection = dataSourceService.connectionByDataSourceId(dataSourceId);
-        boolean success = DbPoolUtil.execute(connection, CommandConstant.KILL_PROCESS + processId);
-        return success ? CommonResult.success().setMessage("线程终止成功") : CommonResult.failed("终止线程失败");
+        try (Connection connection = dataSourceService.connectionByDataSourceId(dataSourceId)) {
+            boolean success = DbPoolUtil.execute(connection, CommandConstant.KILL_PROCESS + processId);
+            return success ? CommonResult.success().setMessage("线程终止成功") : CommonResult.failed("终止线程失败");
+        }
     }
 }
